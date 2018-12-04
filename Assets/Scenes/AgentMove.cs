23effb6daa34d90c9a432073016c01b4da80e696
Araz Yusubov <araz_yusubov@acm.org>
2018-12-04 23:02:29 +0400

    Version 2
    Sound added.

--- Assets/Scenes/AgentMove.cs
+++ Assets/Scenes/AgentMove.cs
@@ -8,10 +8,25 @@ public class AgentMove : MonoBehaviour {
     //public Transform agentGoal;
     public float agentSpeedMin = 0f;
     public float agentSpeedMax = 3f;
+    public float agentHealth = 2f;
+    public float distanceClose = 3f;
 
     private float agentSpeed;
     private NavMeshAgent agent;
     private GameObject player;
+
+    // Audio clips and settings
+    private AudioSource soundSource;
+    private float soundVolLow = .5f;
+    private float soundVolHigh = 1.0f;
+
+    public AudioClip soundKill;
+    public AudioClip soundHitSoft;
+    public AudioClip soundHitHard;
+
+    private void Awake() {
+        soundSource = GetComponent<AudioSource>();
+    }
 
     // Use this for initialization
     void Start () {
@@ -31,9 +46,29 @@ public class AgentMove : MonoBehaviour {
     }
 
     private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag == "bullet") {
-            Destroy(gameObject);
+        if (collision.gameObject.tag == "bullet" && agentHealth > 0) {
+            // Play sound
+            soundSource.pitch = Random.Range(soundVolLow, soundVolHigh);
+            if (Vector3.Distance(gameObject.transform.position, GameObject.Find("Player").transform.position) <= distanceClose) {
+                //Debug.Log("Hard shot!");
+                soundSource.PlayOneShot(soundHitHard);
+            }
+            else {
+                //Debug.Log("Soft shot!");
+                soundSource.PlayOneShot(soundHitSoft);
+            }
+            // Destroy the bullet
             Destroy(collision.gameObject);
+
+            agentHealth--;
+
+            if (agentHealth <= 0) {
+                agent.speed = 0;
+                soundSource.pitch = 1;
+                soundSource.PlayOneShot(soundKill);
+                // Kill it after playing sound
+                Destroy(gameObject, soundKill.length);
+            }
         }
     }
 }
